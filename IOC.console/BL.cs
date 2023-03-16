47c3b6a06f15b2ff01c665abaad0bd5aa899a12b
serhatyagci <serhatyagci1@gmail.com>
2023-03-16 22:38:17 +0300

    Add project files.

---
+++ IOC.console/BL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC.console
+{
+    public class BL
+    {
+        private DataAccessLayer _dal { get; set; }
+        public BL()
+        {
+            _dal= new DataAccessLayer();
+        }
+        public List<Product> GetProducts()
+        {
+            return _dal.GetProducts();
+        }
+    }
+}
---
+++ IOC.console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IOC.console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BL bl = new BL();
+
+            bl.GetProducts().ForEach(x=> 
+            {
+                Console.WriteLine($"{x.Id}-{x.Name}-{x.Price}-{x.Stock}");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
